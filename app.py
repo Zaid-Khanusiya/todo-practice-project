93680aadf0911d2a0cab9e1f3b95ff3e014ace1a
Zaid-Khanusiya <zaidkhanusiya007@gmail.com>
2025-10-16 08:19:03 +0530

    Added requirements.txt & Online DB  support

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_restful import Api
+from dotenv import load_dotenv
+# import os
+
+load_dotenv()
+
+# SUPABASE_DB_URI = os.getenv("SUPABASE_DB_URI")
+# print(SUPABASE_DB_URI)
 
 myapp = Flask(__name__)
 api = Api(myapp)
 
 from routes import *
 
-# Run the app on localhost 5000 port
 if __name__ == '__main__':
-    myapp.run(port=5000, debug=True)
+    myapp.run(port=6262, debug=True, host='0.0.0.0')
--- database.py
+++ database.py
@@ -1,9 +1,15 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 from app import myapp
 
 db = SQLAlchemy()
+SUPABASE_DB_URI = os.getenv("SUPABASE_DB_URI")
+# print(SUPABASE_DB_URI)
 
-myapp.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://zaid:0000@localhost/todo'
+# online supabase database linking connection
+myapp.config['SQLALCHEMY_DATABASE_URI'] = SUPABASE_DB_URI
+
+# myapp.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://zaid:0000@localhost/todo'
 myapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(myapp)
